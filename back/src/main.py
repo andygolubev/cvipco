887d8ba8af9783a4c02f829342ad9ec153605c29
Andy Golubev <verkomf@gmail.com>
2023-01-25 17:49:51 +0200

    make it runnable

--- back/src/main.py
+++ back/src/main.py
@@ -1,1 +1,16 @@
-from fastapi inport FastAPI
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message":"Hello world, FastAPI!"}
+
+
+def main():
+    print("Hello World")
+    uvicorn.run(app, host="0.0.0.0", port=80)
+
+if __name__ == "__main__":
+    main()
